4178f7dbceea0ee237d3971004c145c01bfd622f
João Victor <jv782063@gmail.com>
2020-09-27 19:13:24 -0300

    does the first level 1 exercise

---
+++ exercicios_nivel_1/exercicio1/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x, y, z) // 1. uma única declaração print
+
+	fmt.Println(x)
+	fmt.Println(y) // 2. Múltiplas declarações print
+	fmt.Println(z)
+}
