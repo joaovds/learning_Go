249dd0d5477cb5837cc545b40229d594db2c0a1b
João Victor <jv782063@gmail.com>
2020-10-06 09:49:55 -0300

    does the second level 2 exercise, working with comparison operators

---
+++ exercicios_nivel_2/exercicio2/main.go
@@ -0,0 +1,14 @@
+// operadores de comparação...atribuir os resultados à variáveis
+
+package main
+
+func main() {
+	a := (10 == 100)
+	b := (15 != 15)
+	c := (17 <= 17)
+	d := (12 < 10)
+	e := (10 >= 8)
+	f := (15 > 13)
+
+	println(a, b, c, d, e, f)
+}
