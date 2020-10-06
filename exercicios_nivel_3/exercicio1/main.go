086ed117c71d2c19506c165f69c415b5a961ef32
João Victor <jv782063@gmail.com>
2020-10-06 12:34:43 -0300

    does all level 3 exercises, focusing on repetition loops and conditions

---
+++ exercicios_nivel_3/exercicio1/main.go
@@ -0,0 +1,9 @@
+// Laço de repetição. Colocar em tela os números 1 até 10.000
+
+package main
+
+func main() {
+	for i := 0; i <= 10000; i++ {
+		println(i)
+	}
+}
---
+++ exercicios_nivel_3/exercicio10/main.go
@@ -0,0 +1,14 @@
+// Criar um programa que teste as expressões
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+	fmt.Println(!false)
+}
