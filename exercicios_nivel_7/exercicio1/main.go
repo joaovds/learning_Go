6c02ffad7eac2b1ba138398c082a5b150d06f13f
João Victor <jv782063@gmail.com>
2020-10-08 15:28:26 -0300

    does all level 7 exercises, learning about pointers

---
+++ exercicios_nivel_7/exercicio1/main.go
@@ -0,0 +1,13 @@
+// Trabalhando com Ponteiros
+
+package main
+
+import "fmt"
+
+func main() {
+	valor := 27
+	fmt.Println(&valor)
+}
+
+// - Crie um valor e atribua-o a uma variável.
+// - Demonstre o endereço deste valor na memória.
